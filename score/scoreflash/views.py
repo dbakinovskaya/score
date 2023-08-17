8e022da593bddbabfe0dd8ef85609c201bb5ddbb
Андрей Крылов <numinga27@yandex.ru>
2023-08-17 13:47:24 +0400

    синхронизировал EventId и Event

--- score/scoreflash/views.py
+++ score/scoreflash/views.py
@@ -28,6 +28,10 @@ class EventIdViewSet(viewsets.ModelViewSet):
     serializer_class = EventLiveIdSerializer
     # permission_classes = AllowAny
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_lock = threading.Lock()
+
     def list(self, request):
         # Удаление данных из таблицы
         EventId.objects.all().delete()
@@ -159,69 +163,79 @@ class TournamentViewSet(viewsets.ModelViewSet):
     queryset = Tournament.objects.all()
     serializer_class = TournamentSerializer
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_lock = threading.Lock()
+
     def send_request(self):
-        with transaction.atomic():
-            Tournament.objects.all().delete()
-            Events.objects.all().delete()
+        with self.db_lock:
+            with transaction.atomic():
+                Tournament.objects.all().delete()
+                Events.objects.all().delete()
 
-            url = "https://flashlive-sports.p.rapidapi.com/v1/events/live-list"
-            headers = {
-                'X-RapidAPI-Key': "c68d4d6ac2mshe98277d48f502dbp188062jsn10858273d528",
-                'X-RapidAPI-Host': "flashlive-sports.p.rapidapi.com"
-            }
-            params = {
-                'timezone': '-4',
-                'sport_id': '1',
-                'locale': 'en_INT'
-            }
+                url = "https://flashlive-sports.p.rapidapi.com/v1/events/live-list"
+                headers = {
+                    'X-RapidAPI-Key': "c68d4d6ac2mshe98277d48f502dbp188062jsn10858273d528",
+                    'X-RapidAPI-Host': "flashlive-sports.p.rapidapi.com"
+                }
+                params = {
+                    'timezone': '-4',
+                    'sport_id': '1',
+                    'locale': 'en_INT'
+                }
 
-            response = requests.get(url, headers=headers, params=params)
-            parsed_data = response.json()
-            print(parsed_data)
-            for item in parsed_data['DATA']:
-                tournament = Tournament()
-                tournament.name = item['NAME']
-                tournament.tournament_stage_type = item['TOURNAMENT_STAGE_TYPE']
-                tournament.tournament_imng = item['TOURNAMENT_IMAGE']
-                tournament.TOURNAMENT_TEMPLATE_ID = item['TOURNAMENT_TEMPLATE_ID']
-                tournament.save()
+                response = requests.get(url, headers=headers, params=params)
+                parsed_data = response.json()
+                print(parsed_data)
+                for item in parsed_data['DATA']:
+                    tournament = Tournament()
+                    tournament.name = item['NAME']
+                    tournament.tournament_stage_type = item['TOURNAMENT_STAGE_TYPE']
+                    tournament.tournament_imng = item['TOURNAMENT_IMAGE']
+                    tournament.TOURNAMENT_TEMPLATE_ID = item['TOURNAMENT_TEMPLATE_ID']
+                    tournament.save()
 
-                for event in item['EVENTS']:
-                    data = {
-                        'event_id': event['EVENT_ID'],
-                        'start_time': event['START_TIME'],
-                        'start_utime': event['START_UTIME'],
-                        'game_time': event['GAME_TIME'],
-                        'short_name_away': event['SHORTNAME_AWAY'],
-                        'away_name': event['AWAY_NAME'],
-                        'away_score_current': event['AWAY_SCORE_CURRENT'],
-                        'away_score_part_1': event['AWAY_SCORE_PART_1'],
-                        'away_score_part_2': event.get('AWAY_SCORE_PART_2', ''),
-                        'short_name_home': event['SHORTNAME_HOME'],
-                        'home_name': event['HOME_NAME'],
-                        'home_score_current': event['HOME_SCORE_CURRENT'],
-                        'home_score_part_1': event['HOME_SCORE_PART_1'],
-                        'home_score_part_2': event.get('HOME_SCORE_PART_2', ''),
-                        'home_images': event.get('HOME_IMAGES'),
-                        'away_images': event.get('AWAY_IMAGES'),
-                    }
-                    serializer = EventsSerializer(data=data)
-                    if serializer.is_valid():
-                        event_object = serializer.create(
-                            serializer.validated_data)
-                        tournament.events.add(event_object)
-                    else:
-                        print(serializer.errors)
+                    for event in item['EVENTS']:
+                        data = {
+                            'event_id': event['EVENT_ID'],
+                            'start_time': event['START_TIME'],
+                            'start_utime': event['START_UTIME'],
+                            'game_time': event['GAME_TIME'],
+                            'short_name_away': event['SHORTNAME_AWAY'],
+                            'away_name': event['AWAY_NAME'],
+                            'away_score_current': event['AWAY_SCORE_CURRENT'],
+                            'away_score_part_1': event['AWAY_SCORE_PART_1'],
+                            'away_score_part_2': event.get('AWAY_SCORE_PART_2', ''),
+                            'short_name_home': event['SHORTNAME_HOME'],
+                            'home_name': event['HOME_NAME'],
+                            'home_score_current': event['HOME_SCORE_CURRENT'],
+                            'home_score_part_1': event['HOME_SCORE_PART_1'],
+                            'home_score_part_2': event.get('HOME_SCORE_PART_2', ''),
+                            'home_images': event.get('HOME_IMAGES'),
+                            'away_images': event.get('AWAY_IMAGES'),
+                        }
+                        serializer = EventsSerializer(data=data)
+                        if serializer.is_valid():
+                            event_object = serializer.create(
+                                serializer.validated_data)
+                            tournament.events.add(event_object)
+                        else:
+                            print(serializer.errors)
 
     def start_scheduling(self):
         while True:
-            self.send_request()
+            with self.db_lock:
+                self.send_request()
             time.sleep(5)
 
     def list(self, request):
         threading.Thread(target=self.start_scheduling).start()
 
         tournaments = Tournament.objects.all()
+        # вызов EventIdViewSet
+        event_viewset = EventIdViewSet()
+        events = event_viewset.list(request)
+
         serializer = self.serializer_class(tournaments, many=True)
         return Response(serializer.data)
 
